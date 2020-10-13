7b512d2c426eca5369aed2a5933ca78395b77c81
Johnson <johnson@gmail.com>
2020-10-12 18:56:57 -0700

    added nav

--- soleil_luna/src/components/nav.js
+++ soleil_luna/src/components/nav.js
@@ -1,42 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { FaAlignRight } from 'react-icons/fa';
 
-class Nav extends React.Component {
-     
-    render() {
 
-        return (
-            <nav className='nav' >
-                <ul className='nav-list'>
-                    <li>
-                        <button className='nav-btn btn'> 
-                        Manicure
-                        </button>
-                    </li>
-                       
-                    <li>
-                        <button className='nav-btn btn'>
-                        Pedicare
-                        </button>
-                    </li>
-                    <li>
-                        <button className='nav-btn btn'>
-                        Nail Enhancement
-                        </button>
-                    </li>
-                    <li>
-                        <button className='nav-btn btn'>
-                        Massage
-                        </button>
-                    </li>
-                    <li>
-                        <button className='nav-btn btn'>
-                        Waxing
-                        </button>
-                    </li>                  
+ class Nav extends Component {
+  state = {
+      toggle:false
+  }
+  Toggle = () => {
+      this.setState({toggle:!this.state.toggle})
+  }
+  render() {
+    const li = [
+        {
+            link: "/",
+            text:"Manicure"
+        },
+        {
+            link: "/about/",
+            text:"Pedicure"
+        },
+        {
+            link: "/",
+            text:"Massage"
+        },
+        {
+            link: "/about/",
+            text:"Waxing"
+        },      {
+            link: "/",
+            text:"Nail Enhancement"
+        },
+        {
+            link: "/",
+            text:"DIY Soleil Kit"
+        },
+        {
+            link: "/contact/",
+            text:"About Us"
+        },
+        {
+            link: "/",
+            text:"Gift Cards"
+        }
+      
+    ];
+    return (
+    <>
+        <div className="navBar">
+                <button onClick={this.Toggle}>
+                    <FaAlignRight />
+                </button>
+                <ul className={this.state.toggle ? "links show-nav" : "links"}>
+                    {
+                        li.map((objLink, i) => {
+                            return ( <li><a href={objLink.link}>{objLink.text}</a></li> )
+                        })
+                    }
                 </ul>
-            </nav>
-        );
-    };
-};
+        </div>
+    </>
+    );
+  }
+}
 
 export default Nav;
